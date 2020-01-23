d2830d0cfb9887e98612441d3df390721cf12212
Jon Jagger <jon@jaggersoft.com>
2020-01-23 15:13:50 +0000

    Skip up/down scripts

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -5,7 +5,7 @@ readonly SH_DIR="$( cd "$( dirname "${0}" )/sh" && pwd )"
 source "${SH_DIR}/versioner_env_vars.sh"
 export $(versioner_env_vars)
 "${SH_DIR}/build_docker_images.sh"
-"${SH_DIR}/containers_up.sh"
-echo TODO: now run hiker-like check_red_amber_green.sh does...
-"${SH_DIR}/containers_down.sh"
+#"${SH_DIR}/containers_up.sh"
+#echo TODO: now run hiker-like check_red_amber_green.sh does...
+#"${SH_DIR}/containers_down.sh"
 "${SH_DIR}/on_ci_publish_image.sh"
