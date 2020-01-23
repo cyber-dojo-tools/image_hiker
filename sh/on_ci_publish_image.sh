2a4d59eba9db0be55f8b3c48b55302f0aec57207
Jon Jagger <jon@jaggersoft.com>
2020-01-23 15:02:49 +0000

    Add context to circleci/config.yml

--- sh/on_ci_publish_image.sh
+++ sh/on_ci_publish_image.sh
@@ -12,7 +12,7 @@ on_ci_publish_tagged_images()
   fi
   echo 'on CI so publishing image'
   # DOCKER_USER, DOCKER_PASS are in ci context
-  echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
+  echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
   docker push "$(image_name)"
   docker logout
 }
