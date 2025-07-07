9f8459926c637c6e3ffc4cd8febab4860d982e0e
JonJagger <jon@kosli.com>
2025-07-07 09:30:44 +0100

    Use built-in docker compose

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -5,7 +5,7 @@ readonly ROOT_DIR="$( cd "$( dirname "${0}" )/.." && pwd )"
 # - - - - - - - - - - - - - - - - - - - - - - -
 build_docker_images()
 {
-  docker-compose \
+  docker compose \
     --file "${ROOT_DIR}/docker-compose.yml" \
     build \
     hiker
